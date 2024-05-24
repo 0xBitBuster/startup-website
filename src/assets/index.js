be1d0e7f3e4c0e52afaf593f36a779fb935d96d6
0xBitBuster <121769397+0xBitBuster@users.noreply.github.com>
2024-05-24 11:50:04 +0200

    Refactored asset imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,145 +1,84 @@
-import openai from "./openai.svg";
-import check from "./check.svg";
-import openaiWhiteSymbol from "./openai-symbol-white.svg";
-import play from "./play.svg";
-import gradient from "./gradient.png";
-import smallSphere from "./4-small.png";
-import grid from "./grid.png";
-import check2 from "./check-02.svg";
-import loading1 from "./loading-01.svg";
-import yourlogo from "./yourlogo.svg";
-import homeSmile from "./home-smile.svg";
-import file02 from "./file-02.svg";
-import searchMd from "./search-md.svg";
-import plusSquare from "./plus-square.svg";
-import recording03 from "./recording-03.svg";
-import recording01 from "./recording-01.svg";
-import disc02 from "./disc-02.svg";
-import chromecast from "./chrome-cast.svg";
-import sliders04 from "./sliders-04.svg";
-import loading from "./loading.png";
-import background from "./background.jpg";
+// Hero Section
+export { default as curve } from './hero/curve.png';
+export { default as robot } from './hero/robot.jpg';
+export { default as heroBackground } from './hero/hero-background.jpg';
 
-import curve from "./hero/curve.png";
-import robot from "./hero/robot.jpg";
-import heroBackground from "./hero/hero-background.jpg";
+// Collaboration Section
+export { default as curve1 } from './collaboration/curve-1.svg';
+export { default as curve2 } from './collaboration/curve-2.svg';
+export { default as discord } from './collaboration/discord.png';
+export { default as figma } from './collaboration/figma.png';
+export { default as framer } from './collaboration/framer.png';
+export { default as notion } from './collaboration/notion.png';
+export { default as photoshop } from './collaboration/photoshop.png';
+export { default as protopie } from './collaboration/protopie.png';
+export { default as raindrop } from './collaboration/raindrop.png';
+export { default as slack } from './collaboration/slack.png';
 
-import curve1 from "./collaboration/curve-1.svg";
-import curve2 from "./collaboration/curve-2.svg";
-import discord from "./collaboration/discord.png";
-import figma from "./collaboration/figma.png";
-import framer from "./collaboration/framer.png";
-import notion from "./collaboration/notion.png";
-import photoshop from "./collaboration/photoshop.png";
-import protopie from "./collaboration/protopie.png";
-import raindrop from "./collaboration/raindrop.png";
-import slack from "./collaboration/slack.png";
+// Services Section
+export { default as service1 } from './services/service-1.png';
+export { default as service2 } from './services/service-2.png';
+export { default as service3 } from './services/service-3.png';
 
-import service1 from "./services/service-1.png";
-import service2 from "./services/service-2.png";
-import service3 from "./services/service-3.png";
+// Pricing Section
+export { default as lines } from './pricing/lines.svg';
+export { default as stars } from './pricing/stars.svg';
 
-import lines from "./pricing/lines.svg";
-import stars from "./pricing/stars.svg";
+// Roadmap Section
+export { default as coins } from './roadmap/coins.png';
+export { default as done } from './roadmap/done.svg';
+export { default as hero } from './roadmap/hero.png';
+export { default as roadmap1 } from './roadmap/image-1.png';
+export { default as roadmap2 } from './roadmap/image-2.png';
+export { default as roadmap3 } from './roadmap/image-3.png';
+export { default as roadmap4 } from './roadmap/image-4.png';
+export { default as undone } from './roadmap/undone.svg';
 
-import coins from "./roadmap/coins.png";
-import done from "./roadmap/done.svg";
-import hero from "./roadmap/hero.png";
-import roadmap1 from "./roadmap/image-1.png";
-import roadmap2 from "./roadmap/image-2.png";
-import roadmap3 from "./roadmap/image-3.png";
-import roadmap4 from "./roadmap/image-4.png";
-import undone from "./roadmap/undone.svg";
+// Notification Section
+export { default as notification1 } from './notification/image-1.png';
+export { default as notification2 } from './notification/image-2.png';
+export { default as notification3 } from './notification/image-3.png';
+export { default as notification4 } from './notification/image-4.png';
 
-import notification1 from "./notification/image-1.png";
-import notification2 from "./notification/image-2.png";
-import notification3 from "./notification/image-3.png";
-import notification4 from "./notification/image-4.png";
+// Benefits Section
+export { default as benefitCard1 } from './benefits/card-1.svg';
+export { default as benefitCard2 } from './benefits/card-2.svg';
+export { default as benefitCard3 } from './benefits/card-3.svg';
+export { default as benefitCard4 } from './benefits/card-4.svg';
+export { default as benefitCard5 } from './benefits/card-5.svg';
+export { default as benefitCard6 } from './benefits/card-6.svg';
+export { default as benefitIcon1 } from './benefits/icon-1.svg';
+export { default as benefitIcon2 } from './benefits/icon-2.svg';
+export { default as benefitIcon3 } from './benefits/icon-3.svg';
+export { default as benefitIcon4 } from './benefits/icon-4.svg';
+export { default as benefitImage2 } from './benefits/image-2.png';
 
-import benefitCard1 from "./benefits/card-1.svg";
-import benefitCard2 from "./benefits/card-2.svg";
-import benefitCard3 from "./benefits/card-3.svg";
-import benefitCard4 from "./benefits/card-4.svg";
-import benefitCard5 from "./benefits/card-5.svg";
-import benefitCard6 from "./benefits/card-6.svg";
-import benefitIcon1 from "./benefits/icon-1.svg";
-import benefitIcon2 from "./benefits/icon-2.svg";
-import benefitIcon3 from "./benefits/icon-3.svg";
-import benefitIcon4 from "./benefits/icon-4.svg";
-import benefitImage2 from "./benefits/image-2.png";
+// Socials Section
+export { default as discordBlack } from './socials/discord.svg';
+export { default as facebook } from './socials/facebook.svg';
+export { default as instagram } from './socials/instagram.svg';
+export { default as telegram } from './socials/telegram.svg';
+export { default as twitter } from './socials/twitter.svg';
 
-import discordBlack from "./socials/discord.svg";
-import facebook from "./socials/facebook.svg";
-import instagram from "./socials/instagram.svg";
-import telegram from "./socials/telegram.svg";
-import twitter from "./socials/twitter.svg";
-
-export {
-    openai,
-    check,
-    check2,
-    loading1,
-    openaiWhiteSymbol,
-    play,
-    gradient,
-    smallSphere,
-    grid,
-    yourlogo,
-    homeSmile,
-    file02,
-    searchMd,
-    plusSquare,
-    recording03,
-    recording01,
-    disc02,
-    chromecast,
-    sliders04,
-    loading,
-    background,
-    curve,
-    robot,
-    heroBackground,
-    curve1,
-    curve2,
-    discord,
-    figma,
-    framer,
-    notion,
-    photoshop,
-    protopie,
-    raindrop,
-    slack,
-    service1,
-    service2,
-    service3,
-    lines,
-    stars,
-    coins,
-    done,
-    hero,
-    roadmap1,
-    roadmap2,
-    roadmap3,
-    roadmap4,
-    undone,
-    notification1,
-    notification2,
-    notification3,
-    notification4,
-    benefitCard1,
-    benefitCard2,
-    benefitCard3,
-    benefitCard4,
-    benefitCard5,
-    benefitCard6,
-    benefitIcon1,
-    benefitIcon2,
-    benefitIcon3,
-    benefitIcon4,
-    benefitImage2,
-    discordBlack,
-    facebook,
-    instagram,
-    telegram,
-    twitter,
-};
+// General Imports
+export { default as openai } from './openai.svg';
+export { default as check } from './check.svg';
+export { default as check2 } from './check-02.svg';
+export { default as loading1 } from './loading-01.svg';
+export { default as openaiWhiteSymbol } from './openai-symbol-white.svg';
+export { default as play } from './play.svg';
+export { default as gradient } from './gradient.png';
+export { default as smallSphere } from './4-small.png';
+export { default as grid } from './grid.png';
+export { default as yourlogo } from './yourlogo.svg';
+export { default as homeSmile } from './home-smile.svg';
+export { default as file02 } from './file-02.svg';
+export { default as searchMd } from './search-md.svg';
+export { default as plusSquare } from './plus-square.svg';
+export { default as recording03 } from './recording-03.svg';
+export { default as recording01 } from './recording-01.svg';
+export { default as disc02 } from './disc-02.svg';
+export { default as chromecast } from './chrome-cast.svg';
+export { default as sliders04 } from './sliders-04.svg';
+export { default as loading } from './loading.png';
+export { default as background } from './background.jpg';
